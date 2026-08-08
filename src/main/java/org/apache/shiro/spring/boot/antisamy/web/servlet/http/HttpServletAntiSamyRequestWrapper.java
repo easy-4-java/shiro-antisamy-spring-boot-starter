f7808a9e1a0041be76dcf0e019b609a51f268f58
wandl-6A72h <hiwepy@gmail.com>
2026-08-08 23:51:57 +0800

    chore: propagate pom standardization, tests, javadoc and @author fix from 4.1.x
    - Add jacoco-maven-plugin 0.8.15 with 90% coverage gate
    - Add central-publishing-maven-plugin 0.11.0
    - Update surefire (enable tests, argLine for JaCoCo, *_Test.java pattern)
    - Fix SCM URLs
    - Copy test files from 4.1.x branch
    - Copy Javadoc improvements from 4.1.x branch
    - Standardize @author to [@Loong Wan](https://github.com/loong10k)
    - Propagated to 3.2.x branch

--- src/main/java/org/apache/shiro/spring/boot/antisamy/web/servlet/http/HttpServletAntiSamyRequestWrapper.java
+++ src/main/java/org/apache/shiro/spring/boot/antisamy/web/servlet/http/HttpServletAntiSamyRequestWrapper.java
@@ -15,7 +15,7 @@ import org.apache.shiro.spring.boot.antisamy.utils.XssScanUtils;
 
 /**
  * RichText XSS(Cross Site Scripting)，即跨站脚本攻击请求过滤
- * @author 		： <a href="https://github.com/hiwepy">hiwepy</a>
+ * @author [@Loong Wan](https://github.com/loong10k)
  */
 public class HttpServletAntiSamyRequestWrapper extends HttpServletRequestWrapper {
 
